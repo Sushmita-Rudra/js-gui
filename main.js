5c6970060c92c7aeca2df0e81d6fdff77cfdb040
Sushmmita-Rudra <S537491@NWMISSOURI.EDU>
2019-09-30 10:39:04 -0500

    Added few changes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const add = (x, y) => { return x + y }
+const multiply = (x, y, z) => { return x * y * z }
 
 const validate = async (event) => {
   console.log(`triggered validate on ${event.target.id}`)
@@ -13,31 +13,24 @@ const validate = async (event) => {
 
 const updateWithAdd = async (event) => {
   document.querySelector('#result').innerHTML = ''
-  if (document.querySelector('#firstNumber').checkValidity() && document.querySelector('#secondNumber').checkValidity()) {
+  if (document.querySelector('#height').checkValidity() && document.querySelector('#width').checkValidity()&& document.querySelector('#depth').checkValidity()) {
     const regex = /[^a-zA-Z_]/g
-    const s = document.querySelector('#guest').value.replace(regex, '')
-    const i = parseInt(document.querySelector('#firstNumber').value)
-    const j = parseInt(document.querySelector('#secondNumber').value)
-    const ans = `${s}, your sum is ${add(i, j)}.`
+    const s = parseInt(document.querySelector('#height').value)
+    const i = parseInt(document.querySelector('#width').value)
+    const j = parseInt(document.querySelector('#depth').value)
+    const ans = `Your volume is ${multiply(s, i, j)}.`
     document.querySelector('#result').innerHTML = ans
   }
 }
 
-const updateWithJoke = async (event) => {
-  document.querySelector('#result').innerHTML = ''
-  const url = 'https://api.icndb.com/jokes/random?limitTo=[nerdy]'
-  const response = await fetch(url)
-  const obj = await response.json()
-  const joke = obj.value.joke || 'No joke for you.'
-  document.querySelector('#result').innerHTML = joke
-}
 
 // delegate to dynamic elements (e.g. when testing)
 // focusout is like blur, but it bubbles up
 
 document.addEventListener('focusout', event => {
-  if ((event.target && event.target.id === 'firstNumber') ||
-    (event.target && event.target.id === 'secondNumber')) {
+  if ((event.target && event.target.id === 'height') ||
+    (event.target && event.target.id === 'width')||
+    (event.target && event.target.id === 'depth')) {
     validate(event)
   }
 })
@@ -46,6 +39,3 @@ document.addEventListener('click', event => {
   if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
 })
 
-document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'getJokeButton') { updateWithJoke(event) }
-})
